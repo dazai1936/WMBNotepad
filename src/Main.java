6d150e6fbde1cae59377fe3aca9c8734186b3c0d
sangwoo <whiz10@naver.com>
2025-07-18 14:54:14 +0900

    fix: Prevent Scanner instance duplication

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main{
         }
         else
         {
-            m.write();
+            m.write(sc);
         }
     }
 }
--- src/Memo.java
+++ src/Memo.java
@@ -5,20 +5,23 @@ public class Memo {
     String Date;
     String Content;
 
-    void write() { //작성
-        //다시 스캔을 불러와 내용 작성
+    void write(Scanner sc) { //작성
+        System.out.println("제목을 입력하세요.");
+        Title = sc.nextLine();
+
+        System.out.println("내용을 입력하세요.");
+        Content = sc.nextLine();
     }
     void read() { //읽기
         System.out.println(Content);
     }
-    void edit() { //편집
+    void edit(Scanner sc) { //편집
         System.out.println(Content);
-        Scanner sc = new Scanner(System.in);
         System.out.print("수정할 내용을 입력하세요.");
         Content = sc.nextLine();
     }
 
-    void delete() {}
+    void delete() { //삭제
 
-
+    }
 }
