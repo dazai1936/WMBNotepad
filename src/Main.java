33a9bb5043896aa4d7767b65aef3579800604c44
sangwoo <whiz10@naver.com>
2025-07-19 19:52:58 +0900

    fix Main method

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,31 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Memo m = new Memo();
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("WMBNotepad에 오신 걸 환영합니다.");
         System.out.println("1. 새로 작성");
         System.out.println("2. 목록");
         System.out.println("3. 종료");
 
-        Memo m = new Memo();
-        Scanner sc = new Scanner(System.in); // Scanner 객체 생성
-        String str = sc.next(); // 입력받은 문자열을 str에 저장
+        while (true) {
 
-        switch (str) {
-            case "1":
-                m.write(sc);
-                break;
+            String str = sc.next();
 
-            case "2":
-                //목록
-                break;
-            case "3":
-                //종료
-                break;
-            default:
-                System.out.println("잘못된 입력입니다.");
-                break;
+            switch (str) {
+                case "1":
+                    m.write(sc);
+                    break;
+                case "2":
+                    //목록 출력
+                    break;
+                case "3":
+                    System.out.println("프로그램을 종료합니다.");
+                    System.exit(0);
+                default:
+                    System.out.println("잘못된 입력입니다.");
+            }
         }
     }
 }
